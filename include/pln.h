573f963cdc2322f844378972a57a4ab64368767b
yuxianjiang <yuxianjiang@tencent.com>
2023-07-31 19:31:22 +0800

    围绕vm_run进行信息的组织和代码编写。 vm_run传入的是经过优化后的可执行信息的集合，输出的是执行的结果

---
+++ include/pln.h
@@ -0,0 +1,13 @@
+#ifndef _PLN_H_
+#define _PLN_H_
+
+class Pln
+{
+  enum Type {
+  };
+private :
+  enum Type type;
+};
+
+#endif //_PLN_H_
+
---
+++ include/vm.h
@@ -0,0 +1,8 @@
+#ifndef _VM_H_
+#define _VM_H_
+
+#include "pln.h"
+int
+vm_run(Pln *pln);
+
+#endif //_VM_H_
