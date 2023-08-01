3d29afd15f10ce26a2338dae7cd47d300acc0c34
yuxianjiang <yuxianjiang@tencent.com>
2023-08-01 08:39:23 +0800

    pln interface

--- include/pln.h
+++ include/pln.h
@@ -9,5 +9,8 @@ private :
   enum Type type;
 };
 
+int
+pln_gen(void *ptn /*in*/, Pln *pln /*out*/);
+
 #endif //_PLN_H_
 
---
+++ sql/pln/pln.cc
@@ -0,0 +1,7 @@
+#include "pln.h"
+
+int
+pln_gen(__attribute__((unused)) void *ptn /*in*/, __attribute__((unused)) Pln *pln /*out*/)
+{
+  return 0;
+}
